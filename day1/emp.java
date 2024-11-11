b345ef8e6d5ea1939ff522c42b71464bc2b8df0c
Matta-sumalatha <sumalatha13904@gmail.com>
2024-11-11 09:43:54 +0530

    Add files via upload

---
+++ day1/emp.java
@@ -0,0 +1,12 @@
+class emp
+{
+public static void main(String args[])
+{
+int eno=16;
+String ename="sumalatha";
+float esal=10044.45f;
+System.out.println("The given employee number is:"+eno);
+System.out.println("The given employee name is:"+ename);
+System.out.println("The given employee salary is:"+esal);
+}
+}
